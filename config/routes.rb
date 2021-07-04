f284c6b2874bb41ef795235414a0bdf163f40344
Onur Kaya <onurkaya8@outlook.com>
2021-07-04 21:53:46 +0300

    init project.

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'articles/new'
+  get 'articles/edit'
+  get 'articles/show'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
